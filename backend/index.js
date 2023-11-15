6fbf4dc31e9349dcaf97b7c2e985f6bf2e66180a
Nino_Tralala <105920067+MRJN2017@users.noreply.github.com>
2023-11-15 23:08:22 +0800

    update index

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,3 @@
 import express from "express";
 import cors from "cors";
 
-
-const app = express();
-app use(cors());
-app use(express.json());
-
-app.listen(5000, ()=> console.log("server up and running..."));
